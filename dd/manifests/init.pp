ef0cafc2492fd0583477aaca96e3201883427bb1
Michael Jon Gardner <vintage910@hotmail.com>
2014-07-09 23:42:40 -0400

    minor alterations to dd, need to find a new name

--- dd/manifests/init.pp
+++ dd/manifests/init.pp
@@ -1,10 +1,15 @@
-class dd (	$pattern,
-		$dirpath = '/var/lib/jenkins/workspace' )
-{
-	tidy { 'jenkins cleanup':
-		path => $dirpath,
-		rmdirs => true,
-		recurse => true,
-		matches => $pattern,
-	}
+# Author:   Michael Jon Gardner
+# Email:    vintage910@hotmail.com
+# Date:     July 9, 2014
+# License:  GPL v3
+class dd
+( $pattern,
+  $dirpath = $dd::params,
+) inherits dd::params {
+  tidy { 'jenkins cleanup':
+    path    => $dirpath,
+    rmdirs  => true,
+    recurse => true,
+    matches => $pattern,
+  }
 }
---
+++ dd/manifests/params.pp
@@ -0,0 +1,8 @@
+# Author:   Michael Jon Gardner
+# Email:    vintage910@hotmail.com
+# Date:     July 9, 2014
+# License:  GPL v3
+class dd::params
+{
+  $dirpath = '/var/lib/jenkins/workspace',
+}
