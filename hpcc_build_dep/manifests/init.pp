8974bc475a13dd5a6328562afaa939a1a4a1089c
Michael Jon Gardner <vintage910@hotmail.com>
2014-07-10 15:14:25 -0400

    modified init.pp and params.pp, array structure to load package, and cmake management

--- hpcc_build_dep/manifests/init.pp
+++ hpcc_build_dep/manifests/init.pp
@@ -4,17 +4,37 @@
 # License:  GPL v3
 #
 class hpcc_build_dep (
-  $dependency_list = $hpcc_build_dep::params::dependency_list,
-  $cmake_src       = $hpcc_build_dep::params::cmake_src,
+  $dependency_list          = $hpcc_build_dep::params::dependency_list,
+  $create_cmake_from_source = $hpcc_build_dep::params::create_cmake_from_source,
+  $cmake_file               = $hpcc_build_dep::params::cmake_file,
 ) inherits hpcc_build_dep::params
 {
-  
+  validate_array($dependency_list)
+  validate_string($cmake_file)
+  validate_bool($create_cmake_from_source)
 
   package { $dependency_list :
     ensure => present,
   }
 
-  if ( $cmake_src ) {
+  if ( $create_cmake_from_source ) {
 
+    $source_dir = regsubst($cmake_file, '.tar.gz','')
+
+    file { 'cmake source':
+      ensure => file,
+      path   => "/tmp/${cmake_file}",
+      source => "puppet:///modules/hpcc_build_dep/${cmake_file}",
+    }
+
+    exec { 'cmake bootstrap and install':
+      cwd         => '/tmp/',
+      command     => "tar -zxf ${cmake_file};cd ${source_dir};./bootstrap;make;make install",
+      refreshonly => true,
+      require     => [Package['make'], Package['gcc']], 
+    }
+     
+    File['cmake source'] ~>
+      Exec['cmake boostrap and install']
   }
 }
--- hpcc_build_dep/manifests/params.pp
+++ hpcc_build_dep/manifests/params.pp
@@ -5,19 +5,41 @@
 #
 class hpcc_build_dep::params
 {
+  $cmake_file = 'cmake-2.8.11.tar.gz'
+
   case $::operatingsystem {
   'Ubuntu': {
+    $create_cmake_from_source = false
     case $::operatingsystemrelease {
-    '12.04': { $dependency_list = [ 'g++', 'gcc', 'make', 'cmake', 'bison', 'flex', 'binutils-dev', 'libldap2-dev', 'libcppunit-dev', 'libicu-dev', 'libxalan110-dev', 'zlib1g-dev', 'libboost-regex-dev', 'libssl-dev', 'libarchive-dev', 'python2.7-dev', 'libv8-dev', 'openjdk-6-jdk', 'libapr1-dev', 'libaprutil1-dev' ] }
-    '13.10': { $dependency_list = [ 'g++', 'gcc', 'make', 'cmake', 'bison', 'flex', 'binutils-dev', 'libldap2-dev', 'libcppunit-dev', 'libicu-dev', 'libxalan-c-dev', 'zlib1g-dev', 'libboost-regex-dev', 'libssl-dev', 'libarchive-dev', 'python2.7-dev', 'libv8-dev', 'openjdk-6-jdk', 'libapr1-dev', 'libaprutil1-dev' ] }
-    '14.04': { $dependency_list = [ 'g++', 'gcc', 'make', 'cmake', 'bison flex', 'binutils-dev', 'libldap2-dev', 'libcppunit-dev', 'libicu-dev', 'libxalan-c-dev', 'zlib1g-dev', 'libboost-regex-dev', 'libssl-dev', 'libarchive-dev', 'python2.7-dev', 'libv8-dev', 'openjdk-6-jdk', 'libapr1-dev', 'libaprutil1-dev', 'libiberty-dev' ] }
+    '12.04': { $dependency_list = [ 'g++', 'gcc', 'make', 'cmake', 'bison', 'flex',
+              'binutils-dev', 'libldap2-dev', 'libcppunit-dev', 'libicu-dev',
+              'libxalan110-dev', 'zlib1g-dev', 'libboost-regex-dev', 'libssl-dev',
+              'libarchive-dev', 'python2.7-dev', 'libv8-dev', 'openjdk-6-jdk',
+              'libapr1-dev', 'libaprutil1-dev' ] }
+    '13.10': { $dependency_list = [ 'g++', 'gcc', 'make', 'cmake', 'bison', 'flex',
+                'binutils-dev', 'libldap2-dev', 'libcppunit-dev', 'libicu-dev',
+                'libxalan-c-dev', 'zlib1g-dev', 'libboost-regex-dev', 'libssl-dev',
+                'libarchive-dev', 'python2.7-dev', 'libv8-dev', 'openjdk-6-jdk',
+                'libapr1-dev', 'libaprutil1-dev' ] }
+    '14.04': { $dependency_list = [ 'g++', 'gcc', 'make', 'cmake', 'bison', 'flex',
+                'binutils-dev', 'libldap2-dev', 'libcppunit-dev', 'libicu-dev',
+                'libxalan-c-dev', 'zlib1g-dev', 'libboost-regex-dev', 'libssl-dev',
+                'libarchive-dev', 'python2.7-dev', 'libv8-dev', 'openjdk-6-jdk',
+                'libapr1-dev', 'libaprutil1-dev', 'libiberty-dev' ] }
     default: { fail('Your version of ubuntu does not have default build dependencies') }
     } # end case Ubuntu ::operatingsystemrelease
   } # end case Ubuntu
   'CentOS': {
+    $create_cmake_from_source = true
     case $::operatingsystemmajrelease {
-    '5':     { $dependency_list = [ 'gcc-c++', 'gcc', 'make', 'bison', 'flex', 'binutils-devel', 'openldap-devel', 'libicu-devel', 'libxslt-devel', 'libarchive-devel', 'boost-devel', 'openssl-devel', 'apr-devel', 'apr-util-devel' ] }
-    '6':     { $dependency_list = [ 'gcc-c++', 'gcc', 'make', 'bison', 'flex', 'binutils-devel', 'openldap-devel', 'libicu-devel', 'libxslt-devel', 'libarchive-devel', 'boost-devel', 'openssl-devel', 'apr-devel', 'apr-util-devel' ]  }
+    '5':     { $dependency_list = [ 'gcc-c++', 'gcc', 'make', 'bison', 'flex',
+                'binutils-devel', 'openldap-devel', 'libicu-devel', 'libxslt-devel',
+                'libarchive-devel', 'boost-devel', 'openssl-devel', 'apr-devel',
+                'apr-util-devel' ] }
+    '6':     { $dependency_list = [ 'gcc-c++', 'gcc', 'make', 'bison', 'flex',
+                'binutils-devel', 'openldap-devel', 'libicu-devel', 'libxslt-devel',
+                'libarchive-devel', 'boost-devel', 'openssl-devel', 'apr-devel',
+                'apr-util-devel' ] }
     default: { fail('Your version of CentOS does not have default build dependencies') }
     } # end case CentOS ::operatingsystemmajrelease
   } # end case CentOS
