2e5e675740421e8bad780cb951b68a4df1555df4
Michael Gardner <vintage910@gmail.com>
2014-07-24 16:12:52 +0000

    changed value for -c

--- customfacts/lib/facter/can_resolve_github.rb
+++ customfacts/lib/facter/can_resolve_github.rb
@@ -1,6 +1,6 @@
 # can_resolve_github.rb
 Facter.add('can_resolve_github') do
   setcode do
-     Facter::Util::Resolution.exec('/bin/bash -c \'ping -c 4 github.com &> /dev/null; [[ $? == 0 ]] && echo true || echo false\'')
+     Facter::Util::Resolution.exec('/bin/bash -c \'ping -c 1 github.com &> /dev/null; [[ $? == 0 ]] && echo true || echo false\'')
    end
 end
