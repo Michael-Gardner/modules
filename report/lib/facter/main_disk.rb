ae2957fccca6c0c33d2ac49c0b392daff8906b51
Michael Jon Gardner <vintage910@hotmail.com>
2014-07-28 15:41:59 -0400

    this one should work

--- report/lib/facter/main_disk.rb
+++ report/lib/facter/main_disk.rb
@@ -3,6 +3,6 @@ Facter.add("main_disk") do
     output = Facter::Util::Resolution.exec('/bin/bash -c \'df -t ext4\'')
     formatArray = output.split()
     formatArray = formatArray.drop(7)
-    value = formatArray.at(5).chomp("%")
+    value = formatArray.at(4).chomp("%")
   end
 end
