2c177cbbd075f250c7ce403f57e7ea6040478127
Michael Jon Gardner <vintage910@hotmail.com>
2014-07-28 16:07:47 -0400

    out of bounds?

--- report/lib/facter/storage_used.rb
+++ report/lib/facter/storage_used.rb
@@ -1,16 +1,16 @@
-## storage_used.rb
-#output_string = Facter::Util::Resolution.exec('/bin/bash -c \'df -t nfs -t ext2 -t ext3 -t ext4\'')
-## split into array and drop the header
-#output_array = output_string.split()
-#output_array.drop(7)
-#dhash = Hash.new
-#while output_array.size > 0
-#  dhash[output_array.at(1).to_s] = output_array.at(5).to_s
-#  output_array.drop(6)
-#end
-#
-#dhash.each do |key, value|
-#  Facter.add(key) do
-#    setcode value
-#  end
-#end
+# storage_used.rb
+output_string = Facter::Util::Resolution.exec('/bin/bash -c \'df -t nfs -t ext2 -t ext3 -t ext4\'')
+# split into array and drop the header
+output_array = output_string.split()
+output_array.drop(7)
+dhash = Hash.new
+while output_array.size > 0
+  dhash[output_array.at(0).to_s] = output_array.at(4).to_s
+  output_array = output_array.drop(6)
+end
+
+dhash.each do |key, value|
+  Facter.add(key) do
+    setcode value
+  end
+end
