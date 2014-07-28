cd817924dffca7ded1bfc744315fc2bbd740549e
Michael Jon Gardner <vintage910@hotmail.com>
2014-07-28 14:27:30 -0400

    debug

--- report/lib/facter/storage_used.rb
+++ report/lib/facter/storage_used.rb
@@ -1,20 +1,20 @@
 # storage_used.rb
 output_string = Facter::Util::Resolution.exec('/bin/bash -c \'df -t nfs -t ext2 -t ext3 -t ext4\'')
 # split into array and drop the header
-/*output_array = output_string.split("\n")
+output_array = output_string.split("\n")
 output_array.drop(1)
-dHash = {}
-until output_array.empty? do
-  key = output_array.at(1)
-  val = output_array.at(5)
-  dHash[key] = val
-  output_array.drop(6)
-end
+#dHash = {}
+#until output_array.empty? do
+#  key = output_array.at(1)
+#  val = output_array.at(5)
+#  dHash[key] = val
+#  output_array.drop(6)
+#end
   
-dHash.each do |key, value|
-  Facter.add("drive_" + key) do
-    setcode do
-      value
-    end
-  end
-end */
+#dHash.each do |key, value|
+#  Facter.add("drive_" + key) do
+#    setcode do
+#      value
+#    end
+#  end
+#end
