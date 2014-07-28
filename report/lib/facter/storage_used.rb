e9e8eb764cb5fc5226914d04465edd392c64c039
Michael Jon Gardner <vintage910@hotmail.com>
2014-07-28 16:13:58 -0400

    fuck ya doggy

--- report/lib/facter/storage_used.rb
+++ report/lib/facter/storage_used.rb
@@ -2,7 +2,7 @@
 output_string = Facter::Util::Resolution.exec('/bin/bash -c \'df -t nfs -t ext2 -t ext3 -t ext4\'')
 # split into array and drop the header
 output_array = output_string.split()
-output_array.drop(7)
+output_array = output_array.drop(7)
 dhash = Hash.new
 while output_array.size > 0
   dhash[output_array.at(0).to_s] = output_array.at(4).to_s
