cee8abbf04fc87d0e9414b70497c33f62f436c90
Michael Jon Gardner <vintage910@hotmail.com>
2014-07-28 15:40:35 -0400

    again, sanity check

--- report/lib/facter/main_disk.rb
+++ report/lib/facter/main_disk.rb
@@ -2,7 +2,7 @@ Facter.add("main_disk") do
   setcode do
     output = Facter::Util::Resolution.exec('/bin/bash -c \'df -t ext4\'')
     formatArray = output.split()
-    formatArray.drop(7)
+    formatArray = formatArray.drop(7)
     value = formatArray.at(5).chomp("%")
   end
 end
