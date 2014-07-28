03eee6bb73d7a4de78e22bf83c47431b25c6b751
Michael Jon Gardner <vintage910@hotmail.com>
2014-07-28 13:32:39 -0400

    again, another small change to split()

--- report/lib/facter/storage_used.rb
+++ report/lib/facter/storage_used.rb
@@ -3,13 +3,12 @@ def getstoragemetrics
   output_string = Facter::Util::Resolution.exec('/bin/bash -c \'df -t nfs -t ext2 -t ext3 -t ext4\'')
   # split into array and drop the header
   output_array = output_string.split("\n")
-  output_array = output_array.drop(1)
-  
 end
 
 
 
 Facter.add('storage_used') do
+  confine :kernel => :linux
   setcode do
     getstoragemetrics
   end
