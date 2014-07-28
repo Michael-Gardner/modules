3cd14a742de066705715e642431b490b42dd95c9
Michael Jon Gardner <vintage910@hotmail.com>
2014-07-28 13:58:35 -0400

    working on output of devices

--- report/lib/facter/storage_used.rb
+++ report/lib/facter/storage_used.rb
@@ -3,6 +3,14 @@ def getstoragemetrics
   output_string = Facter::Util::Resolution.exec('/bin/bash -c \'df -t nfs -t ext2 -t ext3 -t ext4\'')
   # split into array and drop the header
   output_array = output_string.split("\n")
+  output_array.drop(1)
+  device = []
+  pused = []
+  until output_array.empty? do
+    devices.push(output_array.at(1))
+    pused.push(output_array.at(5))
+    output_array.drop(6)
+  end
 end
 
 
