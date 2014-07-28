4215e33941ea0c919962bf8433988873b9e1b59b
Michael Jon Gardner <vintage910@hotmail.com>
2014-07-28 15:25:06 -0400

    debug

--- report/lib/facter/storage_used.rb
+++ report/lib/facter/storage_used.rb
@@ -3,20 +3,14 @@ output_string = Facter::Util::Resolution.exec('/bin/bash -c \'df -t nfs -t ext2 
 # split into array and drop the header
 output_array = output_string.split()
 output_array.drop(7)
-device = Array.new
-pused  = Array.new
-until output_array.empty? do
-  device.push(output_array.at(1).to_s)
-  pused.push(output_array.at(5).to_s)
+dhash = Hash.new
+while output_array.size > 0
+  dhash[output_array.at(1).to_s] = output_array.at(5).to_s
   output_array.drop(6)
 end
 
-i = device.size
-while i > 0 do
-  Facter.add(device.at(i).to_s) do
-    setcode do
-      pused.at(i).to_s
-    end
+dhash.each do |key, value|
+  Facter.add(key) do
+    setcode value
   end
-  i -= 1
 end
