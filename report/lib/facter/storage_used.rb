a4572902b0a5890124e5caf54bff78cc36ef5068
Michael Jon Gardner <vintage910@hotmail.com>
2014-07-28 14:12:10 -0400

    massive alteration to storage_used

--- report/lib/facter/storage_used.rb
+++ report/lib/facter/storage_used.rb
@@ -1,23 +1,18 @@
 # storage_used.rb
-def getstoragemetrics
-  output_string = Facter::Util::Resolution.exec('/bin/bash -c \'df -t nfs -t ext2 -t ext3 -t ext4\'')
-  # split into array and drop the header
-  output_array = output_string.split("\n")
-  output_array.drop(1)
-  device = []
-  pused = []
-  until output_array.empty? do
-    devices.push(output_array.at(1))
-    pused.push(output_array.at(5))
-    output_array.drop(6)
-  end
+output_string = Facter::Util::Resolution.exec('/bin/bash -c \'df -t nfs -t ext2 -t ext3 -t ext4\'')
+# split into array and drop the header
+output_array = output_string.split("\n")
+output_array.drop(1)
+dHash = {}
+until output_array.empty? do
+  dHash[output_array.at(1)] = output_array.at(5)
+  output_array.drop(6)
 end
-
-
-
-Facter.add('storage_used') do
-  confine :kernel => :linux
-  setcode do
-    getstoragemetrics
+  
+dHash.each do |key, value|
+  Facter.add("drive_" + key) do
+    setcode do
+      value
+    end
   end
 end
