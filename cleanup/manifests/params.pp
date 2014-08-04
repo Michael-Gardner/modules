4199aaf3603bf14dc797afee4efd8b311a2724ea
Michael Gardner <vintage910@gmail.com>
2014-08-04 13:16:16 -0400

    modified time for utc, also added documentation into the params file

--- cleanup/manifests/params.pp
+++ cleanup/manifests/params.pp
@@ -2,10 +2,14 @@
 #     d(ay) and w(eek) postfixes are acceptable, with 
 #     a leading integer (no space) that acts as a scalar
 # directory is the base location to clean from
+# cronminute is an array of integers that will fill the crontab
+# cronhour is an array of hours we want our cronjob to run our script at
+#   take note that all our servers are on UTC time, so subtract 4 from 
+#   your desired time, and ensure they're in a 24 hour format
 class cleanup::params
 {
   $age        = '1w'
   $directory  = '/var/lib/jenkins/workspace'
   $cronminute = [0]
-  $cronhour   = [6,18]
+  $cronhour   = [2,14]
 }
